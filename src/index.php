1ba3e15f20aca82aa9e2cf0f9c6d533ffa9740d4
Rene B. Pinto <renebentes@yahoo.com.br>
2026-09-01 15:58:09 -0300

    feat: defina arquivo de versão
    Este arquivo é atualizado durante o processo de geração de release.

--- src/index.php
+++ src/index.php
@@ -1,1 +1,18 @@
-<? echo "test";
+<?php
+
+/**
+ * GovBR Theme Release Please POC
+ *
+ * @copyright  (C) 2026 Rene B. Pinto. <renebentes@yahoo.com.br>
+ * @license    MIT; see LICENSE
+ *
+ * @since       __DEPLOY_VERSION__
+ */
+
+use GovBrTheme\Version;
+
+$version = new Version();
+
+echo 'Version: ' . $version->getFullVersion();
+echo 'Major Version: ' . $version::MAJOR_VERSION;
+echo 'Minor Version: ' . $version::MINOR_VERSION;
